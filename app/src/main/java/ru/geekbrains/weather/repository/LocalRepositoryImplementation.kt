ecae6b97acb6b61e1b22f20d8cb2ba007bd623f0
saityan <naminatosky@gmail.com>
2021-10-02 23:19:44 +0300

    converter history-weather (and vise versa) added

---
+++ app/src/main/java/ru/geekbrains/weather/repository/LocalRepositoryImplementation.kt
@@ -0,0 +1,16 @@
+package ru.geekbrains.weather.repository
+
+import ru.geekbrains.weather.domain.Weather
+import ru.geekbrains.weather.room.HistoryDAO
+import ru.geekbrains.weather.utils.convertHistoryEntityToWeather
+import ru.geekbrains.weather.utils.convertWeatherToHistoryEntity
+
+class LocalRepositoryImplementation (private val localDataSource: HistoryDAO): LocalRepository {
+    override fun getAllHistory(): List<Weather> {
+        return convertHistoryEntityToWeather(localDataSource.getAllHistoryEntities())
+    }
+
+    override fun saveEntity(weather: Weather) {
+        localDataSource.insertHistoryEntity(convertWeatherToHistoryEntity(weather))
+    }
+}
