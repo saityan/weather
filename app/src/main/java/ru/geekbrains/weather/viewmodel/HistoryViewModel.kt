c30b71872f07f599cafc6481850f09b2a10784a8
saityan <naminatosky@gmail.com>
2021-10-03 11:46:32 +0300

    HistoryViewModel added

---
+++ app/src/main/java/ru/geekbrains/weather/viewmodel/HistoryViewModel.kt
@@ -0,0 +1,18 @@
+package ru.geekbrains.weather.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.geekbrains.weather.MyApp
+import ru.geekbrains.weather.repository.LocalRepositoryImplementation
+
+class HistoryViewModel (
+    private val historyLiveDataToObserve: MutableLiveData<AppState> = MutableLiveData(),
+    private val historyRepositoryImplementation: LocalRepositoryImplementation =
+        LocalRepositoryImplementation(MyApp.getHistoryDAO())
+) : ViewModel() {
+
+    fun getAllHistory() {
+        historyLiveDataToObserve.value = AppState.Loading
+        historyLiveDataToObserve.value = AppState.SuccessMain(historyRepositoryImplementation.getAllHistory())
+    }
+}
