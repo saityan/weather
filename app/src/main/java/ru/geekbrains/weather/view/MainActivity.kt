e7c8335ab9a958e88af42cc6d68d7e6755571cc7
saityan <naminatosky@gmail.com>
2021-10-16 11:44:39 +0300

    push notifications introduced

--- app/src/main/java/ru/geekbrains/weather/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/weather/view/MainActivity.kt
@@ -2,7 +2,6 @@ package ru.geekbrains.weather.view
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
